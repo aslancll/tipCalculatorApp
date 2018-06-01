18276d39979af0050d070f68b9d6f8a44d409d09
aslancll <celalaslan@outlook.com>
2018-06-01 15:04:01 -0700

    Initial Commit

---
+++ tipCalculatorApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipCalculatorApp
+//
+//  Created by Celal Aslan on 2018-06-01.
+//  Copyright © 2018 Celal Aslan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
