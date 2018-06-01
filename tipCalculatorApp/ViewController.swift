f563b11eff340b72bd82e57466cc3c80d7883073
aslancll <celalaslan@outlook.com>
2018-06-01 16:10:46 -0700

    initial commit

--- tipCalculatorApp/ViewController.swift
+++ tipCalculatorApp/ViewController.swift
@@ -10,6 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet var billAmountTextField: UITextField!
+    
+    @IBOutlet var calculateTipButton: UIButton!
+    
+    @IBAction func calculateTipAction(_ sender: UIButton) {
+    }
+    
+    @IBOutlet var tipAmountLabel: UILabel!
+    
+    
+    @IBOutlet var tipPercentageTextField: UITextField!
+    
+    
+    
+    
+    
+    
+    var billAmount: Int = 0
+    
+    func calculateTip() -> String {
+        let tipPercentage = (billAmount / 100) * 15
+        return " $\(tipPercentage)"
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
